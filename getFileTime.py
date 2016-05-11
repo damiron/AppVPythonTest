303af0e86d57743a5187c4cb2141294f065434c7
Damian Abalo Miron <wardamo.zero@gmail.com>
2016-05-11 08:56:29 +0200

    New files and new test

--- getFileTime.py
+++ getFileTime.py
@@ -2,3 +2,6 @@ import os
 
 print(os.stat('getFileTime.py').st_mtime)
 print(os.path.getmtime('getFileTime.py'))
+
+print(os.stat('tmp'+os.path.sep+'tmp.txt').st_mtime)
+print(os.path.getmtime('tmp'+os.path.sep+'tmp.txt'))
