ecd9abcd995541690c5a1ad720677f1b2c5ab832
Damian Abalo Miron <wardamo.zero@gmail.com>
2016-05-11 08:59:44 +0200

    second attempt

--- getFileTime.py
+++ getFileTime.py
@@ -1,7 +1,9 @@
 import os
-
-print(os.stat('getFileTime.py').st_mtime)
-print(os.path.getmtime('getFileTime.py'))
-
-print(os.stat('tmp'+os.path.sep+'tmp.txt').st_mtime)
-print(os.path.getmtime('tmp'+os.path.sep+'tmp.txt'))
+time1 = os.stat('getFileTime.py').st_mtime
+time2 = os.path.getmtime('getFileTime.py')
+print(time1)
+print(time2)
+time1 = os.stat('tmp'+os.path.sep+'tmp.txt').st_mtime
+time2 = os.path.getmtime('tmp'+os.path.sep+'tmp.txt')
+print(time1)
+print(time2)
